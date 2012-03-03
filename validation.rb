2c09152e4713c3ef6b2b878db593eb276b715f46
Erik <erik@Eriks-MacBook-Pro-2.local>
2012-03-03 01:16:46 -0800

    changing views directory

--- validation.rb
+++ validation.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 
-set :views, File.dirname(__FILE__) + "/views"
+set :views, Proc.new { File.join(root, "templates") }
 
 messages = ["I LOVE YOU", "YOU'RE DOING THE RIGHT THING", "GOOD JOB",
 "I APPRECIATE YOU", "YOU'RE SO GOOD", "I LOVE THE WAY YOU DO THAT",
