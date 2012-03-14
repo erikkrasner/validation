0e6525835abac1b2591dbe019cbb240dc39671ad
Erik <erik@Eriks-MacBook-Pro-3.local>
2012-03-13 22:12:47 -0700

    getting public folder to show up

--- validation.rb
+++ validation.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 set :views, File.dirname(__FILE__) + "/views"
-set :public_folder, File.dirname(__FILE__) + "/public"
+set :public_folder, "public"
 
 get '/' do
     erb :index
