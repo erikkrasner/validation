3fad410f6848242023a264080ad20b306f92e412
Erik <erik@Eriks-MacBook-Pro-3.local>
2012-03-13 20:56:50 -0700

    trying something simpler

--- validation.rb
+++ validation.rb
@@ -1,13 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 
-set :views, Proc.new { File.join(root, "templates") }
-
-messages = ["I LOVE YOU", "YOU'RE DOING THE RIGHT THING", "GOOD JOB",
-"I APPRECIATE YOU", "YOU'RE SO GOOD", "I LOVE THE WAY YOU DO THAT",
-"DON'T WORRY ABOUT IT"]
 
 get '/' do
-    @message = messages.sample
     erb :index
 end
